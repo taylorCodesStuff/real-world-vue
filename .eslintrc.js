2a602815d15765939ef4fcfd9ac8074fa148982e
Taylor Hammons <taylor.x.hammons@gsk.com>
2019-06-11 13:38:16 -0400

    trying to solve eslint issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     "plugin:vue/essential", 
     "@vue/prettier",
-    "plugin: prettier/recommended"
+    "plugin:prettier/recommended"
   ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,10 +1,14 @@
 <template>
-   
+   <h1 @click="handler(arg, event)"></h1>
 </template>
 
 <script>
 export default {
-    
+     data() {
+         return {
+             key: value
+         }
+     },
 }
 </script>
 
